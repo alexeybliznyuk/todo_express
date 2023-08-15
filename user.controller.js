cb5251ed36ab35fb409b6e32ee4cdb05167c9398
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2023-08-15 10:31:35 +0200

    Create user.controller.js
    user controller js

---
+++ user.controller.js
@@ -0,0 +1,27 @@
+class UserController {
+    async createUser(req, res) {
+//        const {login, password} = req.body 
+//        console.log(login, password)
+        console.log('got post')
+        res.json('ok')
+//        res('ok')
+    }
+    async getUsers(req, res) {
+        
+    }
+    async getUser(req, res) {
+        
+    }
+    async createUser(req, res) {
+        
+    }
+    async updateUser(req, res) {
+        
+    }
+    async deleteUser(req, res) {
+        
+    }
+}
+
+//module.exports = new UserController()
+export default new UserController();
